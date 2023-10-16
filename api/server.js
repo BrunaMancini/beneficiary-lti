753ccbeb0c9310549de113e23f4ca6d11328b223
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-10-16 20:07:02 -0300

    adjusting data formatting

--- api/server.js
+++ api/server.js
@@ -31,30 +31,40 @@ server.post('/api/beneficiaryLTI/tickers-by-date', (req, res) => {
     const endDate = req.header('endDate');
 
     if (!startDate || !endDate) {
-        return res.status(400).json({ error: 'both "startDate" and "endDate" headers are required for filtering.' });
+        return res.status(400).json({ error: 'Both "startDate" and "endDate" headers are required for filtering.' });
     }
 
-    const startDateObj = new Date(startDate);
-    const endDateObj = new Date(endDate);
+    // Parse the start and end dates into Date objects
+    const startDateParts = startDate.split('-');
+    const endDateParts = endDate.split('-');
+
+    if (startDateParts.length !== 3 || endDateParts.length !== 3) {
+        return res.status(400).json({ error: 'Invalid date format. Use "dd-MM-yyyy" format.' });
+    }
+
+    const startDateObj = new Date(startDateParts[2], startDateParts[1] - 1, startDateParts[0]);
+    const endDateObj = new Date(endDateParts[2], endDateParts[1] - 1, endDateParts[0]);
 
     if (isNaN(startDateObj.getTime()) || isNaN(endDateObj.getTime())) {
         return res.status(400).json({ error: 'Invalid date format. Use "dd-MM-yyyy" format.' });
     }
 
+    // Filter the data based on the received startDate and endDate
     const filteredData = data.filter(item => {
-        const itemDate = new Date(item.date);
-        return itemDate >= startDateObj && itemDate <= endDateObj;
+        const itemDateParts = item.date.split('-');
+        if (itemDateParts.length === 3) {
+            const itemDateObj = new Date(itemDateParts[2], itemDateParts[1] - 1, itemDateParts[0]);
+            return itemDateObj >= startDateObj && itemDateObj <= endDateObj;
+        }
+        return false;
     });
-    
-    filteredData.sort((a, b) => {
-        const dateA = a.date.split('-').reverse().join('');
-        const dateB = b.date.split('-').reverse().join('');
-        return dateA.localeCompare(dateB);
-    });
-    
+
+    // Sort the filtered data by date in ascending order
+    filteredData.sort((a, b) => a.date.localeCompare(b.date));
+
     res.json(filteredData);
-    
 });
+
 
 server.use(router);
 
