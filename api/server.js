36dc14e3c2db5c3b7a53e7d1ed1d08b1289a2f17
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-10-16 18:12:00 -0300

    testing new routing again

--- api/server.js
+++ api/server.js
@@ -1,34 +1,33 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.get('/api/beneficiaryLTI/tickers-by-date', (req, res) => {
     const db = router.db;
     const data = db.get('api.beneficiaryLTI.tickers-by-date').value();
-    
+
     const startDate = req.header('startDate');
     const endDate = req.header('endDate');
 
     if (!startDate || !endDate) {
-        return res.status(400).json({ error: 'startDate and endDate are required header parameters.' });
+        return res.status(400).json({ error: '"startDate" and "endDate" headers are required for filtering.' });
     }
 
-    // Filter the data based on the received dates
     const filteredData = data.filter(item => {
-        const itemDate = new Date(item.date); // Assuming your JSON data has a 'date' property
-        return itemDate >= new Date(startDate) && itemDate <= new Date(endDate);
+        const itemDate = item.date;
+        return itemDate >= startDate && itemDate <= endDate;
     });
 
     res.json(filteredData);
 });
 
-server.use(router)
+server.use(router);
 
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+    console.log('JSON Server is running');
+});
 
-module.exports = server
+module.exports = server;
